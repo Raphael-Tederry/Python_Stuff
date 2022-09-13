2992e18ea65bc4addcce0dc574e78fdbdd9956c0
Raphael <rptksmil@gmail.com>
2022-09-14 00:39:11 +0300

    made the "hangman" game against the computer. maby add an ascii drawing later

---
+++ hangman.py
@@ -0,0 +1,49 @@
+import random
+import string
+
+from words import words
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+        print(f'You have {lives} lives left. \tAnd you have used these letters: ', ' '.join(used_letters))
+
+        word_list = [letter if letter in used_letters else '_' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives -= 1
+                print('Letter is not in the word. :(\n')
+
+        elif user_letter in used_letters:
+            print("You already tried that bro -_-\n")
+        else:
+            print('Invalid character. Please try again.\n')
+
+    if lives == 0:
+        print('Sorry you died, The word was ', word)
+    else:
+        print('You successfully guessed the word, ', word, '!!!')
+
+
+if __name__ == '__main__':
+    hangman()
