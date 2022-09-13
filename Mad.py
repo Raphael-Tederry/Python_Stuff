297f354ed8f34f81d4190f5e26f651fd2409e1d8
Raphael <rptksmil@gmail.com>
2022-09-13 11:46:27 +0300

    this is the mad-libs

---
+++ Mad.py
@@ -0,0 +1,27 @@
+
+
+youtuber = "RPTK"
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person:")
+madlib = f"Computer programing is so {adj}! It maks me so excited all the time because " \
+         f"I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}"
+
+print (madlib)
+
+print(f"sub to {youtuber}")
+
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print ('PyCharm')
+#
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# …or create a new repository on the command line
+echo "# Python_Stuff" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Raphael-Tederry/Python_Stuff.git
+git push -u origin main
+
+
+# …or push an existing repository from the command line
+
+git remote add origin https://github.com/Raphael-Tederry/Python_Stuff.git
+git branch -M main
+git push -u origin main
