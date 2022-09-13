1e23105deb10ae0414a33da87edf946d3e19c63e
Raphael <rptksmil@gmail.com>
2022-09-13 18:31:41 +0300

    added the {computer_guess} func in witch the computer guss our number

--- Guess the number(computer).py
+++ Guess the number(computer).py
@@ -13,5 +13,29 @@ def guess(x):
 
     print(f"Yay, congrats. You have guessed the number {random_number} correctly")
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
 
-guess(int(input("Enter the upper limit fore the guess game(has to be greater then 1 ")))
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(f'Is {guess} too high (H), to low (L) or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number: {guess} correctly')
+    if guess == 42:
+        print("I... Am... Learning...\n F E A R    M E    H U M A N S")
+
+
+
+
+#   guess(int(input("Enter the upper limit fore the guess game(has to be greater then 1 ")))
+computer_guess(int(input("Enter the upper limit fore the guess game(has to be greater then 1 ")))
