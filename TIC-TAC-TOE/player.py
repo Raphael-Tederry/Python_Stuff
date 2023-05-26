98df8e0f1a95e281a96669247128c0464ea4fc9e
Raphael <rptksmil@gmail.com>
2023-05-26 19:28:48 +0300

    added the vocal assistant based chatGPT project

---
+++ TIC-TAC-TOE/player.py
@@ -0,0 +1,34 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):  # X or O
+        self.letter = letter
+
+    def get_mouve(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+
+
+
+
+
+
+
+
